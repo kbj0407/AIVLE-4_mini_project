7f39e36e058eaa7786dccaeae0e8e483d67a3b6e
Byeongjae <kbj8351@gmail.com>
2025-12-23 11:33:09 +0900

    Update HealthController.java

--- miniproject4-spring/miniproject04/src/main/java/com/example/miniproject04/controller/HealthController.java
+++ miniproject4-spring/miniproject04/src/main/java/com/example/miniproject04/controller/HealthController.java
@@ -1,8 +1,14 @@
+package com.example.miniproject04.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HealthController {
 
     @GetMapping("/health")
-    public ResponseEntity<?> health() {
+    public ResponseEntity<String> health() {
         return ResponseEntity.ok("ok");
     }
 }
